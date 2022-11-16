1ad693b372f44a0eb1481408018292f2e528f0de
Mike Whaley <michael.d.whaley@gmail.com>
2022-11-15 22:33:12 -0500

    fully functional

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -2,4 +2,10 @@ let x = 1
 let y = 2
 
 // Write your code below this line.
-
+if (x > y) {
+    console.log(x)
+} else if (x === y) {
+    console.log("The values are the same")
+} else {
+    console.log(y)
+}
